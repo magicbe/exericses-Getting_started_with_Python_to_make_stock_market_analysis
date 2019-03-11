1d6c1a2708d22e1f36cacb0c90f01911aefc29c3
Magicbe <magicbe.tw@gmail.com>
2019-03-11 20:05:34 +0800

    update CH07 example

---
+++ CH07/class4.py
@@ -0,0 +1,18 @@
+'''
+
+class 4 string leaning
+
+'''
+
+place = 'Taipei' + ' ' + str(100.70175017510)
+print("地點", place)
+
+count = "x" * 5
+print(count)
+
+print(type(count))
+
+print("字元", len(place))
+
+word = "that's not my homework " + place
+print(word)
