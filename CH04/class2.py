8941e72ccb8a86a064e4a6c7a9b7e8984424b069
Magicbe <magicbe.tw@gmail.com>
2019-03-11 17:50:26 +0800

    update CH04 class exercise

--- CH04/class2.py
+++ CH04/class2.py
@@ -1,3 +1,8 @@
+# Python 小邏輯
+# 某某工廠生產一雙鞋子的成本為 100 元，
+# 在市面上賣 200 元，今天接收到了 3000 筆的訂單，
+# 請用 Python 算出淨利。
+
 ShoesCost = int(input('成本：'))
 MarketPrice = int(input('價格：'))
 Order = int(input('訂單：'))
