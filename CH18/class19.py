a25ca0c96187f790c05cbdb12410e1c35766f103
magicbe <magicbe.tw@gmail.com>
2019-03-14 01:38:13 +0800

    update CH18 ma20, ma50, ma80劃線

--- CH18/class19.py
+++ CH18/class19.py
@@ -12,6 +12,9 @@ data = pd.read_csv(Analysis, parse_dates=True, index_col='Date')
 price = data['Close']
 
 moving_avg = price.rolling(20).mean()
+moving_avg50 = price.rolling(50).mean()
+moving_avg80 = price.rolling(80).mean()
+
 moving_avg_mstd = price.rolling(20).std()
 
 # 五五分
@@ -33,8 +36,12 @@ candlestick = [tuple(x) for x in data[['Date', 'Open', 'High', 'Low', 'Close']].
 candlestick_ohlc(top, candlestick, width=0.5, colorup='r', colordown='g', alpha=0.2)
 
 # top.plot(price, color='b')
-top.plot(moving_avg, color='r')
-top.fill_between(moving_avg.index, moving_avg-2*moving_avg_mstd, moving_avg+2*moving_avg_mstd, color='b', alpha=0.2)
+top.plot(moving_avg, color='r', linewidth=1, alpha=0.7, label='MA20')
+top.plot(moving_avg50, color='b', linewidth=1, alpha=0.7, label='MA50')
+top.plot(moving_avg80, color='g', linewidth=1, alpha=0.7, label='MA80')
+
+top.fill_between(moving_avg.index, moving_avg-2*moving_avg_mstd, moving_avg+2*moving_avg_mstd, color='white', alpha=0.1)
 bottom.bar(data.index, data['Volume'])
 
+top.legend()
 plt.show()
