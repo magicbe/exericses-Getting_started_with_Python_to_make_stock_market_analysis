b4d4655937636c69695a4ac2eb1fbb72cb169291
Magicbe <magicbe.tw@gmail.com>
2019-03-13 21:27:19 +0800

    update CH17 Moving Average

---
+++ CH17/class17.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+import pandas as pd
+import csv
+
+style.use('ggplot')
+Analysis = 'AAPL.csv'
+data = pd.read_csv(Analysis, parse_dates=True, index_col='Date')
+price = data['Close']
+
+moving_avg = price.rolling(20).mean()
+
+plt.plot(price, color='b')
+plt.plot(moving_avg, color='r')
+plt.show()
+
+
